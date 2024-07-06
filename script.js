3528f722b9f57b62b688038176baf82f30169262
keillaarr <keillaarruda9@gmail.com>
2024-07-06 16:47:38 -0300

    Todas as Funcionalidades funcionando

--- script.js
+++ script.js
@@ -69,6 +69,29 @@ const maisVendidos = [
     preco: '$80.00',
     link: 'https://seu-link-curso5-aqui'
   },
+  {
+    imagem: 'assets/curso1.jpg',
+    titulo: 'Título do Curso 5',
+    descricao: 'Descrição breve do curso 5.',
+    preco: '$80.00',
+    link: 'https://seu-link-curso5-aqui'
+  },
+  {
+    imagem: 'assets/curso1.jpg',
+    titulo: 'Título do Curso 5',
+    descricao: 'Descrição breve do curso 5.',
+    preco: '$80.00',
+    link: 'https://seu-link-curso5-aqui'
+  },
+  {
+    imagem: 'assets/curso1.jpg',
+    titulo: 'Título do Curso 5',
+    descricao: 'Descrição breve do curso 5.',
+    preco: '$80.00',
+    link: 'https://seu-link-curso5-aqui'
+  },
+
+ 
   // Adicione mais objetos conforme necessário
 ];
 
@@ -78,15 +101,21 @@ function adicionarCardsMaisVendidos() {
   maisVendidos.forEach(curso => {
       // Criando elementos HTML
       const card = document.createElement('div');
-      card.classList.add('scroll-item', 'bg-white', 'shadow-md', 'rounded-lg', 'overflow-hidden');
+      card.classList.add('card', 'bg-base-100', 'shadow-xl', 'mx-2');
+      card.style.width = '18rem'; // Define a largura fixa do card
+
       card.innerHTML = `
-          <img src="${curso.imagem}" alt="${curso.titulo}" class="w-full h-48 object-cover object-center">
-          <div class="p-4">
-              <h3 class="text-gray-900 font-bold text-lg mb-2">${curso.titulo}</h3>
-              <p class="text-gray-600 text-sm">${curso.descricao}</p>
-              <div class="flex justify-between items-center mt-2">
+          <figure class="px-6 pt-6">
+              <img src="${curso.imagem}" alt="${curso.titulo}" class="rounded-xl h-40 w-full object-cover">
+          </figure>
+          <div class="card-body items-center text-center p-3">
+              <div class="flex justify-between w-full">
+                  <h2 class="card-title text-lg font-bold">${curso.titulo}</h2>
                   <p class="text-blue-gray-900 font-medium">${curso.preco}</p>
-                  <a href="${curso.link}" class="bg-red-500 text-blue-gray-900 hover:bg-blue-gray-900/20 py-2 px-4 rounded-lg font-semibold transition duration-300">
+              </div>
+              <div><p class="text-blue-gray-900 font-medium">${curso.descricao} </div>
+              <div class="card-actions mt-2 w-full flex justify-center">
+                    <a href="${curso.link}" target="_blank" class="bg-red-500 text-white hover:bg-red-600 py-2 px-4 rounded-lg font-semibold transition duration-300">
                       Compre Aqui
                   </a>
               </div>
@@ -96,10 +125,124 @@ function adicionarCardsMaisVendidos() {
   });
 
   // Após adicionar os cards, definir os listeners de scroll
-  const scrollItems = document.querySelectorAll('.scroll-item');
   const scrollContainer = document.getElementById('scrollContainer');
   const leftCarouselButton = document.querySelector('.carousel-left-button');
   const rightCarouselButton = document.querySelector('.carousel-right-button');
+  let scrollPosition = 0;
+
+  function scrollLeft() {
+      scrollContainer.scrollBy({
+          left: -200,
+          behavior: 'smooth'
+      });
+  }
+
+  function scrollRight() {
+      scrollContainer.scrollBy({
+          left: 200,
+          behavior: 'smooth'
+      });
+  }
+
+  leftCarouselButton.addEventListener('click', scrollLeft);
+  rightCarouselButton.addEventListener('click', scrollRight);
+}
+
+// Chama a função para adicionar os cards quando o documento HTML estiver completamente carregado
+document.addEventListener('DOMContentLoaded', adicionarCardsMaisVendidos);
+// Dados simulados dos cursos com desconto (substitua com seus dados reais)
+const cursosComDesconto = [
+  {
+    imagem: 'assets/curso1.jpg',
+    titulo: 'Título do Curso 1',
+    descricao: 'Descrição breve do curso 1.',
+    preco: '$95.00',
+    link: 'https://seu-link-curso1-aqui'
+},
+{
+    imagem: 'assets/carreira-e-desenvolvimento-pessoal.jpg',
+    titulo: 'Título do Curso 2',
+    descricao: 'Descrição breve do curso 2.',
+    preco: '$85.00',
+    link: 'https://seu-link-curso2-aqui'
+},
+{
+  imagem: 'assets/curso-1.png',
+  titulo: 'Título do Curso 3',
+  descricao: 'Descrição breve do curso 3.',
+  preco: '$20.00',
+  link: 'https://seu-link-curso3-aqui'
+},
+{
+  imagem: 'assets/financas-e-negocios.jpeg',
+  titulo: 'Título do Curso 4',
+  descricao: 'Descrição breve do curso 4.',
+  preco: '$100.00',
+  link: 'https://seu-link-curso4-aqui'
+},
+{
+  imagem: 'assets/curso1.jpg',
+  titulo: 'Título do Curso 5',
+  descricao: 'Descrição breve do curso 5.',
+  preco: '$80.00',
+  link: 'https://seu-link-curso5-aqui'
+},
+{
+  imagem: 'assets/curso1.jpg',
+  titulo: 'Título do Curso 5',
+  descricao: 'Descrição breve do curso 5.',
+  preco: '$80.00',
+  link: 'https://seu-link-curso5-aqui'
+},
+{
+  imagem: 'assets/curso1.jpg',
+  titulo: 'Título do Curso 5',
+  descricao: 'Descrição breve do curso 5.',
+  preco: '$80.00',
+  link: 'https://seu-link-curso5-aqui'
+},
+{
+  imagem: 'assets/curso1.jpg',
+  titulo: 'Título do Curso 5',
+  descricao: 'Descrição breve do curso 5.',
+  preco: '$80.00',
+  link: 'https://seu-link-curso5-aqui'
+},
+// Adicione mais objetos conforme necessário
+];
+
+// Função para gerar HTML dos cards e adicionar ao DOM
+function adicionarCardsCursosComDesconto() {
+  const container = document.getElementById('scrollContainer3');
+  cursosComDesconto.forEach(curso => {
+      // Criando elementos HTML
+      const card = document.createElement('div');
+      card.classList.add('card', 'bg-base-100', 'shadow-xl', 'mx-2');
+      card.innerHTML = `
+          <figure class="px-6 pt-6">
+              <img src="${curso.imagem}" alt="${curso.titulo}" class="rounded-xl h-40 w-full object-cover">
+          </figure>
+          <div class="card-body items-center text-center p-3">
+              <div class="flex justify-between w-full">
+                  <h2 class="card-title text-lg font-bold">${curso.titulo}</h2>
+                  <p class="text-blue-gray-900 font-medium">${curso.preco}</p>
+              </div>
+              <div><p class="text-blue-gray-900 font-medium">${curso.descricao}</p></div>
+              <div class="card-actions mt-2 w-full flex justify-center">
+                  <a href="${curso.link}" target="_blank" class="bg-red-500 text-white hover:bg-red-600 py-2 px-4 rounded-lg font-semibold transition duration-300">
+                      Compre Aqui
+                  </a>
+              </div>
+          </div>
+      `;
+      container.appendChild(card);
+  });
+
+  // Após adicionar os cards, definir os listeners de scroll
+  const scrollItems = document.querySelectorAll('.card');
+  const scrollContainer = document.getElementById('scrollContainer3');
+  const leftCarouselButton = document.querySelector('.carousel-left-button3');
+  const rightCarouselButton = document.querySelector('.carousel-right-button3');
   let scrollPosition = 0;
 
   function scrollLeft() {
@@ -130,5 +273,5 @@ function adicionarCardsMaisVendidos() {
   rightCarouselButton.addEventListener('click', scrollRight);
 }
 
-// Chama a função para adicionar os cards quando o documento HTML estiver completamente carregado
-document.addEventListener('DOMContentLoaded', adicionarCardsMaisVendidos);
+// Chama a função para adicionar os cards de cursos com desconto quando o documento HTML estiver completamente carregado
+document.addEventListener('DOMContentLoaded', adicionarCardsCursosComDesconto);
